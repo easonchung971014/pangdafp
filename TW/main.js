9381e70705cc0a8ea6e760b837a82559f4aac996
pangdafp <96124955+easonchung971014@users.noreply.github.com>
2022-02-05 13:43:54 +0800

    Add files via upload

---
+++ TW/main.js
@@ -0,0 +1,78 @@
+const searchButton = document.querySelector("nav .desktop-nav .link-search");
+const closeButton = document.querySelector(".search-container .link-close");
+const desktopNav = document.querySelector(".desktop-nav");
+const searchContainer = document.querySelector(".search-container");
+const overlay = document.querySelector(".overlay");
+
+searchButton.addEventListener("click", () => {
+    desktopNav.classList.add("hide");
+    searchContainer.classList.remove("hide");
+    overlay.classList.add("show");
+})
+
+closeButton.addEventListener("click", () => {
+    desktopNav.classList.remove("hide");
+    searchContainer.classList.add("hide");
+    overlay.classList.remove("show");
+})
+
+overlay.addEventListener("click", () => {
+    desktopNav.classList.remove("hide");
+    searchContainer.classList.add("hide");
+    overlay.classList.remove("show");
+})
+
+
+// Mobile Version
+
+const menuIconContainer = document.querySelector("nav .menu-icon-container");
+const navContainer = document.querySelector(".nav-container");
+
+menuIconContainer.addEventListener("click", () => {
+    navContainer.classList.toggle("active");
+})
+
+
+const searchBar = document.querySelector(".mobile-search-container .search-bar");
+const nav = document.querySelector(".nav-container nav");
+const searchInput = document.querySelector(".mobile-search-container input");
+const cancelBtn = document.querySelector(".mobile-search-container .cancel-btn");
+
+searchInput.addEventListener("click", () => {
+    searchBar.classList.add("active");
+    nav.classList.add("move-up");
+    desktopNav.classList.add("move-down");
+})
+
+cancelBtn.addEventListener("click", () => {
+    searchBar.classList.remove("active");
+    nav.classList.remove("move-up");
+    desktopNav.classList.remove("move-down");
+})
+
+
+function myFunction() {
+  // Declare variables
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("mySearch");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("mySe");
+  li = ul.getElementsByTagName("li");
+  // var input, filter, ul, li, a, i;
+  // input = document.getElementById("mysearch");
+  // filter = input.value.toUpperCase();
+  // ul = document.getElementById("myse");
+  // li = ul.getElementsByTagName("li");
+
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    }
+    else {
+      li[i].style.display = "none";
+    }
+  }
+}
