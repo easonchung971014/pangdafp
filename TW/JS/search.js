7130f92b02dc58eb302c777bd3279b6d11d1fb6e
pangdafp <96124955+easonchung971014@users.noreply.github.com>
2022-02-12 16:49:10 +0800

    Add files via upload

--- TW/JS/search.js
+++ TW/JS/search.js
@@ -36,7 +36,7 @@ function myFunction() {
     for (i = 0; i < li.length; i++) {
       a = li[i].getElementsByTagName("a")[0];
       if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = "";
+        li[i].style.display = "block";
       }
       else {
         li[i].style.display = "none";
